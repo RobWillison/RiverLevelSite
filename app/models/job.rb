c67dad5ffa84c435fa15af10c83539ab38a445a7
Rob Willison <Rob@willison.co.uk>
2017-10-14 21:38:21 +0100

    more work

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,7 @@
 class Job < ApplicationRecord
   def duration
-    return 'Running' if last_start_time > last_run
+    return 'None' if !last_run || !last_start_time
+    return 'Running' if last_start_time && last_run && last_start_time > last_run
     return last_run - last_start_time
   end
 end
