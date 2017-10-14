0da81eec9b449f1ff6297a9746c54dfd9c96d7fe
Rob Willison <Rob@willison.co.uk>
2017-10-14 21:14:53 +0100

    more worl

--- app/controllers/job_controller.rb
+++ app/controllers/job_controller.rb
@@ -1,8 +1,16 @@
 class JobController < ApplicationController
   before_action :authenticate_user!
   before_action :check_admin!
+
   def index
     @jobs = Job.all
+  end
+
+  def create
+    river_id = params['river']
+    Job.create!(call: "TrainingData.updateRiver(#{river_id})", run_frequency: 2, priority: 10)
+    Job.create!(call: "Model.train(#{river_id})", run_frequency: 2, priority: 20)
+    Job.create!(call: "Predict.predict(#{river_id})", run_frequency: 1, priority: 30)
   end
 
   def check_admin!
