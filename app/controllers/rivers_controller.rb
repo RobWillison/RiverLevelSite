caef24d8db71defa8c6a7a6012b805a85a5fc26a
Rob Willison <Rob@willison.co.uk>
2017-12-08 17:34:19 +0000

    more work

--- app/controllers/rivers_controller.rb
+++ app/controllers/rivers_controller.rb
@@ -49,7 +49,7 @@ class RiversController < ApplicationController
     @river_data[:predicted_level] = @river_data[:timestamps].collect do |date|
       predictions = predicted_data.find_all { |pred| pred[0] == date}
       next if predictions.empty?
-      prediction = predictions.find { |pred| pred[2] <= date - 1.hour}
+      prediction = predictions.find { |pred| pred[2] <= date - 24.hour}
       next if prediction.nil?
       {x: prediction[0], y: prediction[1]}
     end
