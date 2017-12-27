0837f8a4f288875e0648809147c8bf8dc0a3298c
RobWillison <Rob@willison.co.uk>
2017-12-27 14:59:40 +0000

    more work

--- app/controllers/acuracy_controller.rb
+++ app/controllers/acuracy_controller.rb
@@ -5,19 +5,22 @@ class AcuracyController < ApplicationController
   def index
     @model_acuracy = []
 
-    configs = ModelConfig.all
-    configs.each do |config|
-      models = config.models
+    all_models = ModelConfig.where(default: 1).collect(&:models).flatten
+    rivers = River.with_station
+    rivers.each do |river|
       model_acuracy = Hash.new(0)
+      models = all_models.select { |model| model.river_id == river.id }
+
       predictions = models.collect(&:predictions).flatten
       predictions.each do |prediction|
         next if prediction.acuracy_info.nil?
-        acuracy_info = JSON.load(prediction.acuracy_info)
+        acuracy_info = JSON.parse(prediction.acuracy_info)
         acuracy_info.each {|k, v| model_acuracy[k] += v}
         model_acuracy['total'] += 1
       end
-      acuracies = model_acuracy.collect { |k, v| (v / model_acuracy['total']).to_i}
-      @model_acuracy << [config, model_acuracy['total'], acuracies]
+      acuracies = model_acuracy.collect { |_k, v| (v / model_acuracy['total']).to_i }
+      next if acuracies.empty?
+      @model_acuracy << [river.id, river.name, model_acuracy['total'], acuracies]
     end
   end
 
